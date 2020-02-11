e66b34af4086a1b68148deff20a32dfc3543e067
An <antr.2201@gmail.com>
2020-02-11 18:13:19 +0100

    changes for ios

--- ios/TestTabsApp/main.m
+++ ios/TestTabsApp/main.m
@@ -9,8 +9,11 @@
 
 #import "AppDelegate.h"
 
+#import "UnityUtils.h"
+
 int main(int argc, char * argv[]) {
   @autoreleasepool {
+    InitArgs(argc, argv);
     return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
   }
 }
