9c96e418f76597760b0472348b9ac51877c257b7
An <antr.2201@gmail.com>
2020-02-12 10:58:19 +0100

    reconfig to a basic RN tabs app without Unity

--- ios/TestTabsApp/main.m
+++ ios/TestTabsApp/main.m
@@ -9,11 +9,8 @@
 
 #import "AppDelegate.h"
 
-#import "UnityUtils.h"
-
 int main(int argc, char * argv[]) {
   @autoreleasepool {
-    InitArgs(argc, argv);
     return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
   }
 }
